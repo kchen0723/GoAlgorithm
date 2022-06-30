7e5b215a291053b86b7fd6d95b7ffab2dd457711
kissinger chen <kchen@aurorasolartech.com>
2022-06-29 20:09:09 -0700

    Add Hanoi version 2 question

--- cmd/GoAlgorithm/HanoiTower.go
+++ cmd/GoAlgorithm/HanoiTower.go
@@ -10,6 +10,32 @@ type Hanoi struct {
 	data []int
 }
 
+func solveHanoiTower2() {
+	var source = Hanoi{name: "source", data: []int{1, 2, 3, 4}}
+	var target = Hanoi{name: "target"}
+	var help1 = Hanoi{name: "help1"}
+	var help2 = Hanoi{name: "help2"}
+	solveHanoiTowerHelp2(&source, &help1, &help2, &target, len(source.data))
+}
+
+func solveHanoiTowerHelp2(source *Hanoi, help1 *Hanoi, help2 *Hanoi, target *Hanoi, sourceDataLength int) {
+	if sourceDataLength == 1 {
+		fmt.Print("moving " + strconv.Itoa(source.data[0]) + " from " + source.name + " to " + target.name)
+		target.data = append([]int{source.data[0]}, target.data...)
+		source.data = source.data[1:]
+		// printAllStack(source, help, target)
+		return
+	}
+
+	// solveHanoiTowerHelp(source, target, help, sourceDataLength-1)
+	lastdata := source.data[0]
+	fmt.Print("moving " + strconv.Itoa(lastdata) + " from " + source.name + " to " + target.name)
+	target.data = append([]int{lastdata}, target.data...)
+	source.data = source.data[1:]
+	// printAllStack(source, help, target)
+	// solveHanoiTowerHelp(help, source, target, sourceDataLength-1)
+}
+
 func solveHanoiTower() {
 	var source = Hanoi{name: "source", data: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}}
 	var target = Hanoi{name: "target"}
