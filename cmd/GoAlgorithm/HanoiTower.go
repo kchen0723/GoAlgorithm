043540d44a4f5c3fb651080437c53a6a6e54b923
kissinger chen <kchen@aurorasolartech.com>
2022-06-29 19:28:37 -0700

    we have to use point here, also we use the sourceDataLength to record how many source data will be moved

--- cmd/GoAlgorithm/HanoiTower.go
+++ cmd/GoAlgorithm/HanoiTower.go
@@ -11,27 +11,24 @@ type Hanoi struct {
 }
 
 func solveHanoiTower() {
-	var source = Hanoi{name: "source", data: []int{1, 2, 3, 4}}
+	var source = Hanoi{name: "source", data: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}}
 	var target = Hanoi{name: "target"}
 	var help = Hanoi{name: "help"}
-	source, help, target = solveHanoiTowerHelp(source, help, target)
+	solveHanoiTowerHelp(&source, &help, &target, len(source.data))
 }
 
-func solveHanoiTowerHelp(source Hanoi, help Hanoi, target Hanoi) (Hanoi, Hanoi, Hanoi) {
-	if len(source.data) == 1 {
+func solveHanoiTowerHelp(source *Hanoi, help *Hanoi, target *Hanoi, sourceDataLength int) {
+	if sourceDataLength == 1 {
 		fmt.Println("moving " + strconv.Itoa(source.data[0]) + " from: " + source.name + " to: " + target.name)
 		target.data = append([]int{source.data[0]}, target.data...)
-		source.data = []int{}
-		return source, help, target
+		source.data = source.data[1:]
+		return
 	}
 
-	lastdata := source.data[len(source.data)-1]
-	copieddata := make([]int, len(source.data)-1)
-	copy(copieddata, source.data[:len(source.data)-1])
-	source.data = copieddata
-	source, target, help = solveHanoiTowerHelp(source, target, help)
-	target.data = append([]int{lastdata}, target.data...)
+	solveHanoiTowerHelp(source, target, help, sourceDataLength-1)
+	lastdata := source.data[0]
 	fmt.Println("moving " + strconv.Itoa(lastdata) + " from: " + source.name + " to: " + target.name)
-	help, source, target = solveHanoiTowerHelp(help, source, target)
-	return source, help, target
+	target.data = append([]int{lastdata}, target.data...)
+	source.data = source.data[1:]
+	solveHanoiTowerHelp(help, source, target, sourceDataLength-1)
 }
