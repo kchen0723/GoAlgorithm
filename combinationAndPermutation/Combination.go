8b14a295c407650a987c4c82f20f8c11ec5f80fc
kissinger <kchen0723@gmail.com>
2025-08-19 12:28:17 -0700

    pass in by point

--- combinationAndPermutation/Combination.go
+++ combinationAndPermutation/Combination.go
@@ -31,23 +31,26 @@ func GetCombationFromDuplicateArray(nums []int, length int) [][]int {
 		return [][]int{}
 	}
 	sort.Ints(nums)
-	return getCombationFromDuplicateArrayHelp(nums, length, 0, []int{})
+	var result [][]int
+	getCombationFromDuplicateArrayHelp(nums, length, 0, []int{}, &result)
+	return result
 }
 
-func getCombationFromDuplicateArrayHelp(nums []int, length int, index int, candidate []int) [][]int {
+func getCombationFromDuplicateArrayHelp(nums []int, length int, index int, candidate []int, result *[][]int) {
 	if len(candidate) == length {
 		item := append([]int(nil), candidate...)
-		return [][]int{item}
+		*result = append(*result, item)
+		return
 	}
-	var result [][]int
+
 	for i := index; i < len(nums); i++ {
 		if i > index && nums[i] == nums[i-1] {
 			continue
 		}
 		candidate = append(candidate, nums[i])
-		items := getCombationFromDuplicateArrayHelp(nums, length, i+1, candidate)
-		result = append(result, items...)
+
+		getCombationFromDuplicateArrayHelp(nums, length, i+1, candidate, result)
+
 		candidate = candidate[:len(candidate)-1]
 	}
-	return result
 }
