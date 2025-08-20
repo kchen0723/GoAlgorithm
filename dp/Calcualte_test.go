46de5e5c8a46b009a6f292aaa3d7988be4e7ba39
kissinger <kchen0723@gmail.com>
2025-08-20 14:13:43 -0700

    fix the bug

--- dp/Calcualte_test.go
+++ dp/Calcualte_test.go
@@ -6,7 +6,9 @@ import (
 )
 
 func TestCalcualte(t *testing.T) {
-	str := "1+((9-3)*4-10)/(6-4)+1"
+	// str := "9-10+11"
+	// str := "1+((9-3)*4-10)/(6-4)+1"
+	str := "1+((3*((9-3)*4-10)/(6-4)+1)+2)/3+5"
 	result := Calcualte(str)
 	fmt.Println(result)
 }
--- dp/Calculate.go
+++ dp/Calculate.go
@@ -8,21 +8,21 @@ func Calcualte(str string) float64 {
 	}
 
 	runes := []rune(str)
-	return calcualteHelp(runes)
+	return calcualteHelp(&runes)
 }
 
-func calcualteHelp(runes []rune) float64 {
+func calcualteHelp(runes *[]rune) float64 {
 	var stack []float64
 	var tokens []rune
 	var num float64
 	sign := '+'
-	for len(runes) > 0 {
-		token := runes[0]
-		runes = runes[1:]
+	for len(*runes) > 0 {
+		token := (*runes)[0]
+		*runes = (*runes)[1:]
 		if (token >= '0' && token <= '9') || token == '.' {
 			tokens = append(tokens, token)
 		}
-		if token == '(' || token == ')' || token == '+' || token == '-' || token == '*' || token == '/' || len(runes) == 0 {
+		if token == '(' || token == ')' || token == '+' || token == '-' || token == '*' || token == '/' || len(*runes) == 0 {
 			if len(tokens) > 0 {
 				num, _ = strconv.ParseFloat(string(tokens), 64)
 				tokens = tokens[:0]
@@ -33,7 +33,7 @@ func calcualteHelp(runes []rune) float64 {
 			switch sign {
 			case '+':
 				stack = append(stack, num)
-			case '_':
+			case '-':
 				stack = append(stack, 0-num)
 			case '*':
 				stack[len(stack)-1] = stack[len(stack)-1] * num
